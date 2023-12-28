4a280a83387a026fa99986b7614a69411f92bd60
musstt <musthofaanwari0110@gmail.com>
2023-12-28 08:11:27 +0700

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/must/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/must/catkin_ws/devel;/home/must/Desktop/basic_ros/devel;/opt/ros/noetic'.split(';') if '/home/must/catkin_ws/devel;/home/must/Desktop/basic_ros/devel;/opt/ros/noetic' != '' else []
